3d5176aa5db0c17931948fb649588120ebd3345b
kbamnote3 <kunalbamnote7@gmail.com>
2025-10-01 11:03:24 +0530

    Update HiringPage with all required components and imports

--- src/components/pages/hirePage/HiringPage.jsx
+++ src/components/pages/hirePage/HiringPage.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import HireHeroComp from './HireHeroComp'
+import AECHiringPage from './AecHiringComp'
+import WhyEliteStandoutComp from './WhyEliteStandoutComp'
+import VarietyOfTalentComp from './VarietyOfTalentComp'
+import TalentForEveryoneComp from './TalentForEveryoneComp'
+import CompaniesComp from './CompaniesComp'
+import HiringWithEliteComp from './HiringWithEliteComp'
+import TestimonalsComp from './TestimonalsComp'
 
 const HiringPage = () => {
   return (
-    <div>HiringPage</div>
+    <div>
+      <HireHeroComp/>
+      <AECHiringPage/>
+      <WhyEliteStandoutComp/>
+      <VarietyOfTalentComp/>
+      <TalentForEveryoneComp/>
+      <CompaniesComp/>
+      <HiringWithEliteComp/>
+      <TestimonalsComp/>
+    </div>
   )
 }
 
