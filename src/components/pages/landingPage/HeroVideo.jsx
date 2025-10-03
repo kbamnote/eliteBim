843e8ac43cf6f38840eb6216f4227dc472ba8142
kbamnote3 <kunalbamnote7@gmail.com>
2025-10-03 10:16:36 +0530

    deploy3

--- src/components/pages/landingPage/HeroVideo.jsx
+++ src/components/pages/landingPage/HeroVideo.jsx
@@ -7,7 +7,7 @@ const HeroVideo = () => {
       {/* Background Video */}
       <video
         className="absolute top-0 left-0 w-full h-full object-cover"
-        src="https://elite-associates-s3.s3.eu-north-1.amazonaws.com/heroVideo.mp4"
+        src="https://elite-india-s3.s3.ap-south-1.amazonaws.com/heroVideo.mp4"
         autoPlay
         loop
         muted
