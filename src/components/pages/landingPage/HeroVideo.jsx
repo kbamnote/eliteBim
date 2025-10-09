5b7a9b82cd6aa77cc3dd75ec0f495c4873e1ae61
kbamnote3 <kunalbamnote7@gmail.com>
2025-10-09 11:48:19 +0530

    video5

--- src/components/pages/landingPage/HeroVideo.jsx
+++ src/components/pages/landingPage/HeroVideo.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import bimVideo from '../../../assets/bimVideo.mp4'
 
 const HeroVideo = () => {
   return (
     <div className="relative w-full overflow-hidden" style={{ height: "calc(100vh - 6rem)" }}>
       <video
         className="absolute inset-0 w-full h-full object-cover"
-        src={bimVideo}
+        src="/bimVideo.mp4"
         autoPlay
         loop
         muted
