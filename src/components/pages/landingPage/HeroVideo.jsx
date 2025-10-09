849ffc03e90cea13096677d78800ab0fe885b49e
kbamnote3 <kunalbamnote7@gmail.com>
2025-10-09 11:17:45 +0530

    video2

--- src/components/pages/landingPage/HeroVideo.jsx
+++ src/components/pages/landingPage/HeroVideo.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import video from "../../../assets/AZnCFLir4FBEChxIN6ExUA-AZnCFLirGS4UcxzWJqhJaw.mp4"
- 
 
 const HeroVideo = () => {
   return (
     <div className="relative w-full overflow-hidden" style={{ height: "calc(100vh - 6rem)" }}>
       <video
         className="absolute inset-0 w-full h-full object-cover"
-        src={video}
+        src="/AZnCFLir4FBEChxIN6ExUA-AZnCFLirGS4UcxzWJqhJaw.mp4"
         autoPlay
         loop
         muted
